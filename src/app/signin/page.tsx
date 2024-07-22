58fa19bdbcc129d4ad5a37967ca5374e7b929f6b
Konstantinos Voskakis <kvoskakis@marpoint.gr>
2024-07-22 17:47:28 +0300

    hashing on password works will need to remove duplicate signin prompt

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,10 @@
+import {SignIn} from '@/components/sign-in';
+
+export default async function SignInPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignIn />
+    </div>
+  );
+}
+  
