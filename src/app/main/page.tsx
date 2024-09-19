30ddd70391786e4f736df1ba2a1604faa2e584d5
Voskakis <constantinosvoskakis@hotmail.gr>
2024-09-19 19:03:46 +0300

    transformed to actual layout

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,6 +4,8 @@ import Dashboard from "@/components/layouts/dashboard";
 
 export default async function SignInPage() {
   return (
-    <Dashboard/>
+    <Dashboard>
+      {/* //TODO: content per link for children*/}
+    </Dashboard>
   );
 }
