e77f396827c8f21b38c0209a42857df5ce3466b1
Voskakis <constantinosvoskakis@hotmail.gr>
2024-09-28 02:27:49 +0300

    beautification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import SigningNav from "@/components/signingNavigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-center flex-grow p-8">
+      {/* Program Name */}
+      <h1 className="text-4xl font-bold mb-8 text-center">
+        Professional Networking Application
+      </h1>
       <SigningNav />
     </main>
   );
