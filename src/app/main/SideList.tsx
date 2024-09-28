2933c6661cf51345b96e0468d6c13407c099b146
Voskakis <constantinosvoskakis@hotmail.gr>
2024-09-28 05:24:16 +0300

    updated sidelist

--- src/app/main/SideList.tsx
+++ src/app/main/SideList.tsx
@@ -2,16 +2,40 @@ import React from "react";
 import {
   Home as HomeIcon,
   ManageAccounts as ManageAccountsIcon,
+  People as PeopleIcon,
+  AdUnits as AdUnitsIcon,
+  Forum as ForumIcon,
+  Notifications as NotificationsIcon
 } from '@mui/icons-material';
 import SideListElement from "@/lib/interfaces/SideListElement";
 
 export const sideList: SideListElement[] = [
   {
-    label: 'Feed',
+    label: 'Home',
     icon: <HomeIcon />,
-    route: '/main/feed'
+    route: '/main/feed',
   },
-]
+  {
+    label: 'Network',
+    icon: <PeopleIcon />,
+    route: '/main/network',
+  },
+  {
+    label: 'Ads',
+    icon: <AdUnitsIcon />,
+    route: '/main/ads',
+  },
+  {
+    label: 'Discussions',
+    icon: <ForumIcon />,
+    route: '/main/discussions',
+  },
+  {
+    label: 'Notifications',
+    icon: <NotificationsIcon />,
+    route: '/main/notifications',
+  },
+];
 
 export const adminSideList: SideListElement[] = [
   {
