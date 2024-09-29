4a0358a024b6154f72b6e571b03e4318a82dd326
Voskakis <constantinosvoskakis@hotmail.gr>
2024-09-29 04:34:05 +0300

    formatting

--- src/app/main/feed/page.tsx
+++ src/app/main/feed/page.tsx
@@ -2,9 +2,6 @@ import Feed from "@/components/Feed";
 
 export default async function FeedPage() {
   return (
-    // <div  className="flex flex-col items-center justify-between p-24">
-    //   feed placeholder
-    // </div>
     <Feed />
   );
 }
--- src/app/main/network/page.tsx
+++ src/app/main/network/page.tsx
@@ -1,7 +1,7 @@
 export default async function NetworkPage() {
-    return (
-      <div  className="flex flex-col items-center justify-between p-24">
-        network placeholder
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-between p-24">
+      network placeholder
+    </div>
+  );
+}
